002495d3d87db964f8cac8b1cb5083d08c811ba9
John Nganga <mukunajohn329@gmail.com>
2024-08-05 21:27:03 +0300

    changes

--- src/app/components/new-posts/new-posts.component.ts
+++ src/app/components/new-posts/new-posts.component.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CategoriesService } from '../../services/categories.service';
 import { HttpClientModule} from '@angular/common/http';
 import { AngularEditorModule } from '@kolkov/angular-editor';
 
+
+
+
 @Component({
   selector: 'app-new-posts',
   standalone: true,
   imports: [RouterModule, CommonModule, FormsModule,
-    AngularEditorModule, HttpClientModule
+    AngularEditorModule, HttpClientModule,  ReactiveFormsModule
   ],
   templateUrl: './new-posts.component.html',
   styleUrl: './new-posts.component.css'
@@ -21,8 +24,26 @@ export class NewPostsComponent implements OnInit {
   imgSrc: any = './assets/image.jpg';
   selectedImg: any;
   categories: any;
+  postForm: FormGroup;
 
-  constructor(private catService: CategoriesService) {}
+  constructor(private catService: CategoriesService, private fb: FormBuilder) {
+
+    this.postForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(10)]],
+      permalink: ['', Validators.required],
+      excerpt: ['', [Validators.required, Validators.minLength(50)]],
+      category: ['', Validators.required],
+      postImg: ['', Validators.required],
+      content: ['', Validators.required],
+    })
+
+  }
+
+  get fc() {
+    return this.postForm.controls;
+  }
+
+  
 
   ngOnInit(): void {
     this.catService.loadData().subscribe(val => {
